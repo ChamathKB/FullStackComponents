26c98e9a1e0ba0eb69a19c2a1b04bd6127b19e28
ChamathKB <amsmckbattanayaka@std.appsc.sab.ac.lk>
2022-06-16 21:11:41 +0530

    added react test

---
+++ 1App10Ways/reactjs/src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useRef, useState } from 'react';
+
+function App() {
+
+  const [todos, setTodos] = useState([]);
+  const todoText = useRef();
+
+  useEffect(() => {
+    const existingTodos = localStorage.getItem('todos');
+    setTodos(existingTodos ? JSON.parse(existingTodos) : []);
+  }, []);
+
+  function addTodo(event) {
+    event.preventDefault();
+    const next = [...todos, todoText.current.value];
+    setTodos(next);
+    localStorage.setItem('todos', JSON.stringify(next));
+  }
+
+  return (
+    <div>
+      <ul>
+        {todos.map(todo => (<li key={todo}>{todo}</li>))}
+      </ul>
+      <form onSubmit={addTodo}>
+         <input type="text" placeholder="What need to be done?" ref={todoText} />
+         <input type="submit" value="Add Todo"/>
+      </form>
+    </div>
+  );
+
+}
+
+export default App;
